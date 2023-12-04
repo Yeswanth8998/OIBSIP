1b51d97e6aab9c5db98a488ae75725467d3e8387
Yeswanth8998 <150122405+Yeswanth8998@users.noreply.github.com>
2023-12-04 13:13:22 +0530

    Add files via upload
    This is a Body Mass Index calculator. This calculates the BMI by taking user of the weight and height. I have developed this for my Oasis Infobyte Python Development Internship.

---
+++ BMICalculator.py
@@ -0,0 +1,28 @@
+def calculate_bmi(weight, height):
+    # Calculate BMI using the formula: BMI = weight (kg) / (height (m) * height (m)
+    bmi = weight / (height ** 2)
+    return bmi
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        return "Normal Weight"
+    elif 25 <= bmi < 29.9:
+        return "Overweight"
+    else:
+        return "Obese"
+
+def main():
+    print("BMI Calculator")
+    weight = float(input("Enter your weight in kilograms: "))
+    height = float(input("Enter your height in meters: "))
+
+    bmi = calculate_bmi(weight, height)
+    category = classify_bmi(bmi)
+
+    print(f"Your BMI is: {bmi:.2f}")
+    print(f"Category: {category}")
+
+if __name__ == "__main__":
+    main()
